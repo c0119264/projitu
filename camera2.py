a8f681be71abca3df644a3c011824dc1f430829b
c0119264 <c0119264@edu.teu.ac.jp>
2022-01-14 23:39:02 +0900

    camera

---
+++ camera2.py
@@ -0,0 +1,13 @@
+from picamera import PiCamerra
+from time import sleep
+from datetime import datetime
+
+
+
+
+camera=PiCamera()
+camera.start_preview()
+sleep(5)
+filename=datetime.now().strftime("%Y-%m-%dT%H%M%S")+".png"
+camera.capture("~/projitu/filename")
+camera.stop_preview()
