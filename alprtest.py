60ce25842f55f95226ef7d3b9f6f85c199ae2620
c0119264 <c0119264@edu.teu.ac.jp>
2022-01-14 23:12:39 +0900

    alpr

--- alprtest.py
+++ alprtest.py
@@ -6,23 +6,18 @@ def res_cmd_lfeed(cmd):
             shell=True).stdout.readlines()
 
 def main():
-    result=[]
     text =str(input("Please register car number: "))
     cmd = ("alpr -n 5 ea7the.jpg")
     data=res_cmd_lfeed(cmd)
-    #print(data)
+    result=[]
     for i in range(1,len(data)):
         if ( text in (data[i].decode())):
-            #print("True")
             result+=["True"]
-            #print("my car")
         else:
-            #print("False")
             result+=["False"]
     if "True" in result:
         print("あなたの車です。")
     else:
-        print("違法駐車の可能性大!!!!")
-
+        print("違法駐車の可能性大!!!")
 if __name__=='__main__':
     main()
